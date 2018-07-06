bc2d04c02d24ed6f11b7e4396423377f3d0e38c1
Edith707 <edith.lopezhuerta@gmail.com>
2018-07-06 00:34:45 -0500

    cambio 1

--- Artemisa-proyecto/src/app/app.module.ts
+++ Artemisa-proyecto/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from "@angular/material";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule {}
+
