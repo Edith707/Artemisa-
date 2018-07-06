e3ff1e1cf471e9a5b0faaa411d0bb049ea727431
Edith707 <edith.lopezhuerta@gmail.com>
2018-07-06 13:37:41 -0500

    hacciendo componentes

--- Artemisa-proyecto/src/app/app.module.ts
+++ Artemisa-proyecto/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from "@angular/material";
+import { RoboComponent } from './robo/robo.component';
+import { AcosoComponent } from './acoso/acoso.component';
+import { ViolacionComponent } from './violacion/violacion.component';
+import { DescargasComponent } from './descargas/descargas.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoboComponent,
+    AcosoComponent,
+    ViolacionComponent,
+    DescargasComponent
   ],
   imports: [
     BrowserModule,
