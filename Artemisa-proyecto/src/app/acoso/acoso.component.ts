438ac41e553debffd9ed54259d35da8168f201e6
Edith707 <edith.lopezhuerta@gmail.com>
2018-08-09 19:44:40 -0500

    change

--- Artemisa-proyecto/src/app/acoso/acoso.component.ts
+++ Artemisa-proyecto/src/app/acoso/acoso.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-acoso',
   templateUrl: './acoso.component.html',
-  styleUrls: ['./acoso.component.css']
+  styleUrls: ['./acoso.component.css'],
+  
 })
 export class AcosoComponent implements OnInit {
 
@@ -11,5 +12,7 @@ export class AcosoComponent implements OnInit {
 
   ngOnInit() {
   }
+  
 
 }
+
